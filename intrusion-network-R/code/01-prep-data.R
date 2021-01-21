a861fc17ccdb920cc03f389a3b6be13b8744848a
Quinn Webber <webber.quinn@gmail.com>
2021-01-21 10:14:58 -0330

    Update 01-prep-data.R

--- intrusion-network-R/code/01-prep-data.R
+++ intrusion-network-R/code/01-prep-data.R
@@ -1,7 +1,33 @@
 
 
 
-library(devtools)
+### Packages ----
+libs <- c('data.table', 'devtools')
+lapply(libs, require, character.only = TRUE)
+
 devtools::install_github("KluaneRedSquirrelProject/krsp")
 
+library(krsp)
+
+con <- krsp_connect(host = "krsp.cepb5cjvqban.us-east-2.rds.amazonaws.com",
+                     dbname ="krsp",
+                     username = Sys.getenv("krsp_user"),
+                     password = Sys.getenv("krsp_password")
+)
+
+
+
+## Pull trapping data
+trp <- tbl(con, "trapping") %>%
+  collect(trapping)
+setDT(trp)
+
+## extract year from date
+trp[, year := year(date)]
+trp[, JDate := yday(date)]
+
+trp$locXnum <- loc_to_numeric(trp$locx)
+trp$locYnum <- loc_to_numeric(trp$locy)
+
+fwrite(trp, "output/trp.csv")
 
