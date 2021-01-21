226bef704d76f178c4a3f0953b36ec786183fb68
Quinn Webber <webber.quinn@gmail.com>
2021-01-21 11:03:53 -0330

    Update 02-MCPs.R

--- intrusion-network-R/code/02-MCPs.R
+++ intrusion-network-R/code/02-MCPs.R
@@ -4,7 +4,8 @@
 ### Packages ----
 libs <- c('data.table', 
           'sp', 'adehabitatHR',
-          'igraph', 'spatsoc')
+          'igraph', 'spatsoc',
+          'ggplot2')
 lapply(libs, require, character.only = TRUE)
 
 utm21N <- '+proj=utm +zone=21 ellps=WGS84'
@@ -15,12 +16,17 @@ source("functions/GetHRBy.R")
 
 
 ## add column to count number of locs 
-trp18 <- trp[gr == "AG" & year == "2018"][,N := .N, by = c("squirrel_id", "gr", "year")]
+trp16 <- trp[gr == "AG" & year == "2016"][,N := .N, by = c("squirrel_id", "gr", "year")]
+
+## remove NAs from coords
+trp16 <- trp16[!is.na(locYnum)]
+trp16 <- trp16[!is.na(locXnum)]
 
 ## generate MCP home ranges
 params = c(grid = 400, extent = 3)
-grd.mcps <- trp18[N > 6][, GetHRBy(squirrel_id, locXnum, locYnum, 95, type = 'mcp')]
+grd.mcps <- trp16[N > 6][, GetHRBy(squirrel_id, locXnum, locYnum, 95, type = 'mcp')]
 
+## visualize MCPs
 ggplot(grd.mcps, aes(x = long, y = lat, 
                          fill = id)) +
   geom_polygon(alpha = 0.25, size = 0.5, color = "black") +
