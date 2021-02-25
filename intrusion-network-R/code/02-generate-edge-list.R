dd7d5994b74c3d6c88b67edd4bd81a06ad5d0419
Quinn Webber <webber.quinn@gmail.com>
2021-02-25 08:29:36 -0330

    Update 02-generate-edge-list.R

--- intrusion-network-R/code/02-generate-edge-list.R
+++ intrusion-network-R/code/02-generate-edge-list.R
@@ -23,21 +23,36 @@ ggplot(df) +
 
 df$squirrel_id <- as.factor(df$squirrel_id)
 
+prj <- '+init=epsg:26911'
 spdf <- SpatialPointsDataFrame(coordinates(cbind(df$locx, df$locy)),
-                              data = df[,c("locx", "locy", "squirrel_id")])
+                              data = df[,c("locx", "locy", "squirrel_id")],
+                                        proj4string = CRS(prj))
 
 source("functions/GetHRBy.R")
 
+## generate ranges by ID
 ud <- setDT(df)[, GetHRBy(squirrel_id, locx, locy, 50 ,"kernel")]
+
+## assign prj
+proj4string(ud) <- CRS(prj)
 
 # Get polygon
 polygon <- st_as_sf(ud)
+
 # convert to sf object
 colnames(polygon) <- c("id_polygons", "area" ,"geometry") # change colnames
-#polygon$id_polygons <- paste0("poly_", polygon$id_polygons) #  change polygon ID
 
 ## output area
 area <- data.table(polygon$id_polygons, polygon$area)
+setnames(area, c("V1", "V2"), c("squirrel_id", "area_ha"))
+
+round(area$area_ha, 8)
+
+ggplot(area) +
+  geom_point(aes(round(area$area_ha, 8), squirrel_id)) +
+  theme(axis.title.y = element_blank())
+
+fwrite(area, "output/territory-area.csv")
 
 ## drop area 
 drops <- c("area") # list of column names
@@ -47,9 +62,9 @@ polygon <- polygon[,!(names(polygon) %in% drops)] #remove columns "name1" and "n
 points <- st_as_sf(spdf)
 
 # Intersection between polygon and points ---------------------------------
-
 intersection <- st_intersection(x = polygon, y = points)
 
+## generate edge list with territory owners and intruders
 edge_list <- data.table(owner = intersection$id_polygons,
                         intruder = intersection$squirrel_id)
 
@@ -65,6 +80,9 @@ edge_list$edge[edge_list$edge == "FALSE"] <- 1
 
 ## subset to only include intrusion events 
 edge_list <- edge_list[edge == 1][,c("edge") := NULL]
+
+
+fwrite(edge_list, "output/edge_list.csv")
 
 
 grph <- graph_from_edgelist(as.matrix(edge_list), directed=T)
