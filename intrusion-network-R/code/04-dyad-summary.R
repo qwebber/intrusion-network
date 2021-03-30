08980bce13ead17fd43451d28a96e4d7f9f495a6
Quinn Webber <webber.quinn@gmail.com>
2021-03-30 10:58:31 -0230

    Update 04-dyad-summary.R

--- intrusion-network-R/code/04-dyad-summary.R
+++ intrusion-network-R/code/04-dyad-summary.R
@@ -5,24 +5,12 @@ libs <- c('data.table',
           'ggplot2', 'krsp')
 lapply(libs, require, character.only = TRUE)
 
-## load pedigree data
-
-con <- krsp_connect(host = "krsp.cepb5cjvqban.us-east-2.rds.amazonaws.com",
-                    dbname ="krsp",
-                    username = Sys.getenv("krsp_user"),
-                    password = Sys.getenv("krsp_password")
-)
-
-ped <- tbl(con, "pedigree_clean") %>%
-  collect()
-setDT(ped)
-
-## remove NAs from dam_id column
-ped <- ped[!is.na(ped$dam_id)]
+## load lifetime data
+life <- fread("output/lifetime_clean.csv")
 
 ## add column for unique dyad ID of moms and offspring
-ped$dyad <- as.factor(paste(ped$squirrel_id, ped$dam_id, sep = "_"))
-ped$mom <- "yes"
+life$dyad <- as.factor(paste(life$squirrel_id, life$dam_id, sep = "_"))
+life$mom <- "yes"
 
 ## load data
 edge_list <- readRDS("output/edge_list.RDS")
@@ -32,7 +20,7 @@ edge_list <- rbindlist(edge_list)
 edge_list$dyad <- as.factor(paste(edge_list$owner, edge_list$intruder, sep = "_"))
 
 ## merge to form file of just moms and their offspring
-mom_offspring <- merge(edge_list, ped[,c("dyad", "mom")], by = "dyad")
+mom_offspring <- merge(edge_list, life[,c("dyad", "mom")], by = "dyad")
 
 ## generate new edgelist file without moms and their offspring
 edge_list <- edge_list %>% 
@@ -98,3 +86,4 @@ sum <- df_edges[, .N, by = c("edge", "behaviour")]
 ddply(sum, c('N'))
 
 
+ggplot()
