3c4b0863ea2f1a6059731d2a9ad69f6e12c7c98f
Quinn Webber <webber.quinn@gmail.com>
2021-03-30 10:58:29 -0230

    Update 03-generate-networks.R

--- intrusion-network-R/code/03-generate-networks.R
+++ intrusion-network-R/code/03-generate-networks.R
@@ -7,16 +7,20 @@ lapply(libs, require, character.only = TRUE)
 
 ## load data
 edge_list <- readRDS("output/edge_list.RDS")
+edge_list$gr_year <- as.factor(paste(edge_list$year, edge_list$grid, sep = "_"))
 yr <- fread("output/unique-grid-years.csv")
 
 ## number of unique grid-years
 n <- length(unique(yr$gr_year))
+gr_year <- unique(yr$gr_year)
 
 ## generate list of spatial points dataframes
 out_mats <- c()
 for(i in 1:n){ 
   
-  df1 <- edge_list[[i]][, .N, by = c("owner", "intruder")]
+  k <- gr_year[i]
+  
+  df1 <- edge_list[gr_year == k][, .N, by = c("owner", "intruder")]
 
   mat <- df1 %>%
           mutate_if(is.character, factor, levels = unique(df1$intruder)) %>%
@@ -29,7 +33,6 @@ for(i in 1:n){
 names(out_mats) <- yr$gr_year
 
 saveRDS(out_mats, "output/matrices/matrix_list.RDS")
-
 
 metrics <- c()
 for(i in 1:n){ 
@@ -45,16 +48,8 @@ for(i in 1:n){
   
   metrics[[i]] <- data.table(outstrength = graph.strength(grph_out, mode = c("out")),
                              instrength = graph.strength(grph_in, mode = c("in")),
-                             outevcent = evcent(grph_out)$vector,
-                             inevcent = evcent(grph_in)$vector,
-                             outbetween = betweenness(grph_out, v = V(grph_out), directed = TRUE,
-                                           nobigint = TRUE, normalized = FALSE),
-                             inbetween = betweenness(grph_in, v = V(grph_in), directed = TRUE,
-                                                      nobigint = TRUE, normalized = FALSE),
                              squirrel_id = names(degree(grph_out)),
-                             squirrel_id_check = names(degree(grph_in)),
-                             obs_out = sum(out_mats_out[[i]]),
-                             obs_in = sum(out_mats_in[[i]]),
+                             #squirrel_id_check = names(degree(grph_in)),
                              gr_year = names(out_mats)[i])
 
 }
