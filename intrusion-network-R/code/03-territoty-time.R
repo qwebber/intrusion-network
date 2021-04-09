8d8c9b180c2779d7cb6ccca22b0e41c4c7536ac0
Quinn Webber <webber.quinn@gmail.com>
2021-04-09 15:10:56 -0230

    Update 03-territoty-time.R

--- intrusion-network-R/code/03-territoty-time.R
+++ intrusion-network-R/code/03-territoty-time.R
@@ -12,25 +12,66 @@ lapply(libs, require, character.only = TRUE)
 edge_list <- readRDS("output/edge_list.RDS")
 edge_list$gr_year <- as.factor(paste(edge_list$year, edge_list$grid, sep = "_"))
 
+## load census
+census <- fread("output/census_all.csv")
+census <- census[!is.na(squirrel_id)]
+## change name of squirrel_id in census to owner to merge with edge_list
+setnames(census, "squirrel_id", "owner")
+census$owner <- as.character(census$owner)
+
+## filter to only include owner and sex variables
+census <- census[,c("owner", "Sex")]
+
+#remove duplicated rows
+census <- census[!duplicated(census)]
+
+## add intruder id
+census$intruder <- census$owner
+
+## add sex of owner and sex of intruder
+edge_list <- merge(census[,c("owner", "Sex")], edge_list, by = "owner", all = T)
+setnames(edge_list, "Sex", "owner_sex")
+edge_list <- merge(census[,c("intruder", "Sex")], edge_list, by = "intruder", all = T)
+setnames(edge_list, "Sex", "intruder_sex")
+
+edge_list <- edge_list[!is.na(owner)][!is.na(intruder)]
+
+edge_list$group <- 1:length(edge_list$owner)
+
+## remove the intruder column
+edge_list_owner <- edge_list[, c("owner", "owner_sex", 
+                                 "locx", "locy", "julian","edge", 
+                                 "year", "dyad", "grid",
+                                 "gr_year", "group")]
+edge_list_owner$direction <- "owner"
+setnames(edge_list_owner, c("owner_sex", "owner"), c("sex", "squirrel_id"))
+## remove the owner column
+edge_list_intruder <- edge_list[, c("intruder", "intruder_sex", 
+                                 "locx", "locy","julian","edge", 
+                                 "year", "dyad", "grid",
+                                 "gr_year", "group")]
+edge_list_intruder$direction <- "intruder"
+setnames(edge_list_intruder, c("intruder_sex", "intruder"), c("sex", "squirrel_id"))
+
 ## add min and max julian days for squirrel territories separately for owners and intruders
-edge_list_owner <- edge_list[edge == 0][, minDay := min(julian), by = c("owner", "grid", "year")][, maxDay := max(julian), by = c("owner", "grid", "year")]
-edge_list_intruder <- edge_list[edge == 1][, minDay := min(julian), by = c("owner", "grid", "year")][, maxDay := max(julian), by = c("owner", "grid", "year")]
-
-edge_list <- rbind(edge_list_owner, edge_list_intruder)
-
-## assign presence of squirrels on territories
+edge_list[, minDay := min(julian), by = c("owner", "grid", "year", "dyad")][, maxDay := max(julian), by = c("owner", "grid", "year", "dyad")]
+edge_list[, minDayOwn := min(julian), by = c("owner", "grid", "year")][, maxDayOwn := max(julian), by = c("owner", "grid", "year")]
+edge_list[, minDayInt := min(julian), by = c("intruder", "grid", "year")][, maxDayInt := max(julian), by = c("intruder", "grid", "year")]
 
 ## 1: presence of a squirrel within the lifetime of the territory (i.e. true intrusion)
 
+edge_list[, during := minDayInt > minDayOwn & maxDayInt < maxDayOwn & edge == 1]
+edge_list[, before := minDayInt < minDayOwn & edge == 1]
+edge_list[, after := maxDayInt > maxDayOwn & edge == 1]
+edge_list[during == "TRUE"]
 
-sq12613 <- edge_list[owner == 5194]
+9444 + 156660 + 11584
 
-ggplot(sq12613) +
-  geom_jitter(aes(julian, year, color = owner), 
-              height = 0.01, alpha = 0.25) + 
-  geom_errorbar(aes(y=year, xmin=minDay, xmax=maxDay, color = owner), 
-                width=0.2, size=0.1) +
-  facet_wrap(~edge)
+aa <- edge_list[owner == 12613]
+ggplot(aa[year == 2016]) +
+  geom_jitter(aes(julian, intruder, color = intruder), 
+              height = 0.01, alpha = 0.25)  +
+  geom_errorbar(aes(y=intruder, xmin=minDay, xmax=maxDay, color = intruder), 
+                width=0.2, size=0.1, height = 0.2) 
+  facet_wrap(~year)
 
-edge_list[, index := if(get(julian) > node_split) node_child_left 
-          else index , by = seq_len(nrow(test))]
