bf770b4a394a46e58a180dc707eda0e564a80407
Quinn Webber <webber.quinn@gmail.com>
2021-04-12 17:10:08 -0230

    get_spdf function

---
+++ intrusion-network-R/functions/get_spdf.R
@@ -0,0 +1,22 @@
+
+
+get_spdf <- function(df, n){
+
+## df = input dataframe
+## n = levels for the loop
+    
+out <- c()
+
+for(i in levels(n)){ 
+  
+  df2 <- df[gr_year == i] ## this is very specific to my dataset and not easily transferable - so, must have a colulmn called "gr_yeaR" to run
+  
+  spdf <- SpatialPointsDataFrame(coordinates(cbind(df2$locx, df2$locy)), ## locs must be "locx" and "locy" to run
+                                 data = df2,
+                                 proj4string = CRS(prj))
+  
+  out[[i]] <- st_as_sf(spdf)
+  
+}
+out
+}
