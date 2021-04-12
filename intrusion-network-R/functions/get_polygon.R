a0234cedaa72d7e88ac3581bfbe5509d152ec7b4
Quinn Webber <webber.quinn@gmail.com>
2021-04-12 17:22:38 -0230

    add get_polygon function

---
+++ intrusion-network-R/functions/get_polygon.R
@@ -0,0 +1,38 @@
+
+
+get_polygon <- function(df, n, in.percent, params) {
+  
+  ## df = input dataframe
+  ## n = levels for the loop
+  ## id = identity of animals
+  ## in.percent = kernel %
+  ## params = parameters for kernels (see GetHRBy function)
+  
+  out_polygon <- c()
+  
+  for(i in levels(n)){ 
+    
+    df3 <- df[gr_year == i]
+    
+    ## generate ranges by ID
+    ud <- df3[, GetHRBy(squirrel_id, 
+                        locx, locy, ## coords must be locx and locy
+                        in.percent = in.percent, 
+                        params = params,
+                        type = "kernel")]
+    
+    ## assign prj
+    proj4string(ud) <- CRS(prj)
+    
+    # Get polygon
+    polygon <- st_as_sf(ud)
+    
+    # convert to sf object
+    colnames(polygon) <- c("id_polygons", "area" ,"geometry") # change colnames
+    
+    out_polygon[[i]] <- polygon
+    
+  }  
+  
+  out_polygon
+}
