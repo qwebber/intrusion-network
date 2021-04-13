6317ab0668884e0d7ab6100bec4a847b6b88bcb9
Quinn Webber <webber.quinn@gmail.com>
2021-04-13 10:07:41 -0230

    get_intersection function

---
+++ intrusion-network-R/functions/get_intersection.R
@@ -0,0 +1,22 @@
+
+
+## function written by Quinn Webber to generate overlap between red squirrel territory polygons and points
+
+get_intersection <- function(poly, spdf, n) {
+  
+  ## poly = list of polygons
+  ## spdf = list of spdf 
+  ## n = levels for the loop
+
+  # Intersection between polygon and points ---------------------------------
+  intersect_out <- c()
+  for(i in 1:length(n)){
+  
+  intersection <- st_intersection(x = poly[[i]], y = spdf[[i]])
+  
+  intersect_out[[i]] <- intersection 
+  }
+
+  intersect_out
+    
+}
