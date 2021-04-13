5540ceb833928ef8acaca154ca5ca4df89521254
Quinn Webber <webber.quinn@gmail.com>
2021-04-13 10:47:05 -0230

    Update 01-cone_counts.R

--- intrusion-network-R/code/generate auxilliary data/01-cone_counts.R
+++ intrusion-network-R/code/generate auxilliary data/01-cone_counts.R
@@ -4,7 +4,7 @@
 #Andrew McAdam
 
 
-library(tidyverse)
+library(dplyr)
 library (krsp)
 
 #con <- krsp_connect(group="krsp-aws")
@@ -35,19 +35,20 @@ cone_counts<-tbl(con, "cones") %>%
 ##################################
 # Means calculated per Grid Year #
 ##################################
-cones_grids_years <- group_by(cone_counts, Year, Grid) %>% 
-  summarise(num_trees = sum(!is.na(NumNew)),
+cones_grids_years <- cone_counts %>% 
+  dplyr::group_by(Year, Grid) %>% 
+  dplyr::summarise(num_trees = sum(!is.na(NumNew)),
             cone_counts = mean(NumNew, na.rm = TRUE),
             cone_index = mean(cone_index, na.rm = TRUE)) %>% 
-  mutate(Year_tp1 = Year + 1,
+  dplyr::mutate(Year_tp1 = Year + 1,
          cone_index_t = ifelse(is.finite(cone_index), cone_index, NA))
 
 #link in cones from the previous year
 cone_temp<-cones_grids_years %>% 
-  select(Grid, Year, Year_tp1, cone_index_tm1=cone_index_t)
+  dplyr::select(Grid, Year, Year_tp1, cone_index_tm1=cone_index_t)
 
 cones_grids_years<-left_join(cones_grids_years, cone_temp, by=c("Grid", "Year" = "Year_tp1")) %>% 
-  select(-Year_tp1, -Year.y)
+  dplyr::select(-Year_tp1, -Year.y)
 
 # Manually code mast years
 
@@ -87,6 +88,7 @@ cones_grids_years<-cones_grids_years %>%
   mutate (EXP_label = 1) %>% 
   mutate (EXP_label = ifelse(Exp=="f", 19, EXP_label))
 
+saveRDS(cones_grids_years, "output/auxilliary-data/cones_grids_years.RDS")
 
 #############################
 # Means calculated per Year #
@@ -94,10 +96,10 @@ cones_grids_years<-cones_grids_years %>%
 
 
 yearly_cones <- group_by(cone_counts, Year) %>% 
-  summarize(num_trees = sum(!is.na(NumNew)),
-            cone_counts = mean(NumNew, na.rm = TRUE),
-            cone_index = mean(cone_index, na.rm = TRUE),
-            total_cones = mean(total_cones, na.rm = TRUE))
+  dplyr::summarise(num_trees = sum(!is.na(NumNew)),
+                   cone_counts = mean(NumNew, na.rm = TRUE),
+                   cone_index = mean(cone_index, na.rm = TRUE),
+                   total_cones = mean(total_cones, na.rm = TRUE))
 
 # Must add in data for 1989 because there were no cones but the zeros were not entered
 yearly_cones <- rbind(yearly_cones,
@@ -126,10 +128,10 @@ cone_counts_klsu<-tbl(con, "cones") %>%
   mutate(total_cones = 1.11568 * exp(0.1681 + 1.1891 * log(NumNew + 0.01)))
 
 yearly_cones_klsu <- group_by(cone_counts_klsu, Year) %>% 
-  summarize(num_trees = sum(!is.na(NumNew)),
-            cone_counts = mean(NumNew, na.rm = TRUE),
-            cone_index = mean(log(NumNew + 1), na.rm = TRUE),
-            total_cones = mean(total_cones, na.rm = TRUE))
+  dplyr::summarise(num_trees = sum(!is.na(NumNew)),
+                   cone_counts = mean(NumNew, na.rm = TRUE),
+                   cone_index = mean(log(NumNew + 1), na.rm = TRUE),
+                   total_cones = mean(total_cones, na.rm = TRUE))
 
 yearly_cones_klsu <- yearly_cones_klsu %>% 
   mutate(cone_index_t = ifelse(is.finite(cone_index), cone_index, NA)) %>% 
