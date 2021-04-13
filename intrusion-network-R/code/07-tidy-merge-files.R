511ba8d41ccf3f38cdf40eeca9518ed5861633fc
Quinn Webber <webber.quinn@gmail.com>
2021-04-13 11:02:56 -0230

    Update 07-tidy-merge-files.R

--- intrusion-network-R/code/07-tidy-merge-files.R
+++ intrusion-network-R/code/07-tidy-merge-files.R
@@ -8,7 +8,7 @@ lapply(libs, require, character.only = TRUE)
 
 
 ## load lifetime data
-life <- fread("output/lifetime_clean.csv")
+life <- readRDS("output/auxilliary-data/lifetime-clean.RDS")
 
 ## load network metrics
 metrics <- fread("output/metrics.csv")
@@ -16,7 +16,7 @@ metrics$gr_year <- as.factor(paste(metrics$grid, metrics$year, sep = "_"))
 
 
 ## load annual density
-density <- fread("output/spring-density.csv")
+density <- readRDS("output/auxilliary-data/spring-density.RDS")
 density$gr_year <- as.factor(paste(density$grid, density$year, sep = "_"))
 
 all <- merge(metrics, density[,c("V1", "year", "grid") := NULL], by = "gr_year")
