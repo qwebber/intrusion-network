9b388a17759e56511f62017dd1889024896cb731
Quinn Webber <webber.quinn@gmail.com>
2021-04-14 11:16:49 -0230

    Update 07-tidy-merge-files.R

--- intrusion-network-R/code/07-tidy-merge-files.R
+++ intrusion-network-R/code/07-tidy-merge-files.R
@@ -7,8 +7,20 @@ libs <- c('data.table',
 lapply(libs, require, character.only = TRUE)
 
 
-## load lifetime data
-life <- readRDS("output/auxilliary-data/lifetime-clean.RDS")
+## load database
+con <- krsp_connect (host = "krsp.cepb5cjvqban.us-east-2.rds.amazonaws.com",
+                     dbname ="krsp",
+                     username = Sys.getenv("krsp_user"),
+                     password = Sys.getenv("krsp_password")
+)
+
+flastall <- tbl(con, "flastall2") %>% 
+  #flastall2 contains juveniles that were not tagged
+  # exclusions
+  filter(gr %in% c("SU", "KL")) %>% 
+  select(squirrel_id, gr, sex, byear=byear, dam_id, bcert=bcert)
+
+flastall <- setDT(collect(flastall))
 
 ## load network metrics
 metrics <- fread("output/metrics.csv")
@@ -19,8 +31,13 @@ metrics$gr_year <- as.factor(paste(metrics$grid, metrics$year, sep = "_"))
 density <- readRDS("output/auxilliary-data/spring-density.RDS")
 density$gr_year <- as.factor(paste(density$grid, density$year, sep = "_"))
 
-all <- merge(metrics, density[,c("V1", "year", "grid") := NULL], by = "gr_year")
+all <- merge(metrics, setDT(density)[,c("year", "grid") := NULL], by = "gr_year")
 
-#merge(all, life[,c("sex", "byear", "squirrel_id")], by = "squirrel_id", fill = T)
+## merge metrics, density, and other life-history information
+all <- merge(all, flastall, by = "squirrel_id", fill = T)
 
-fwrite(all, "output/final-df.csv")
+## calculate age
+all$age <- all$year - all$byear
+
+saveRDS(all, "output/final-df.RDS")
+
