2043e291278411e85a2b17831faee5325241607f
Quinn Webber <webber.quinn@gmail.com>
2021-04-19 08:01:47 -0230

    Update GetHRBy.R

--- intrusion-network-R/functions/GetHRBy.R
+++ intrusion-network-R/functions/GetHRBy.R
@@ -8,7 +8,7 @@ GetHRBy <- function(by.col, x.col, y.col, in.percent, type, params = NULL){
   #crs(ids) <- in.crs
   if(type == 'kernel'){
     kernel <- do.call(kernelUD, c(ids, params))
-    vert <- getverticeshr(kernel, percent = in.percent, unout = "ha")  
+    vert <- getverticeshr(kernel, percent = in.percent, unout = "m2")  
   } else if(type == 'mcp'){
     vert <- do.call(mcp, c(xy = ids, percent = in.percent, params))
   } else {
