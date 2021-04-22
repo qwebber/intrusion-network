7d1222038db1dcabbc3f3480d8b55e7bd0f5d432
Quinn Webber <webber.quinn@gmail.com>
2021-04-22 09:21:22 -0230

    Update get_spdf.R

--- intrusion-network-R/functions/get_spdf.R
+++ intrusion-network-R/functions/get_spdf.R
@@ -2,16 +2,18 @@
 
 ## function written by Quinn Webber to converte red squirrel locs to spdf format
 
-get_spdf <- function(df, n){
+get_spdf <- function(df, n, yr){
   
   ## df = input dataframe
   ## n = levels for the loop
     
   out <- c()
 
-  for(i in levels(n)){ 
+  for(i in 1:n){ 
   
-  df2 <- df[gr_year == i] ## this is very specific to my dataset and not easily transferable - so, must have a colulmn called "gr_yeaR" to run
+  k = as.character(yr[i])  
+    
+  df2 <- df[gr_year == k] ## this is very specific to my dataset and not easily transferable - so, must have a colulmn called "gr_yeaR" to run
   
   spdf <- SpatialPointsDataFrame(coordinates(cbind(df2$locx, df2$locy)), ## locs must be "locx" and "locy" to run
                                  data = df2,
