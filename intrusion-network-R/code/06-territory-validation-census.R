d498bb03765fba6ff18cae3a0b590c4d0f4a20bc
Quinn Webber <webber.quinn@gmail.com>
2021-04-23 14:13:34 -0230

    Update 06-territory-validation-census.R

--- intrusion-network-R/code/06-territory-validation-census.R
+++ intrusion-network-R/code/06-territory-validation-census.R
@@ -10,6 +10,16 @@ polys <- readRDS("output/edge-list-inputs/polygons-all.RDS")
 
 ## load census data
 census_all <- readRDS("output/auxilliary-data/census-all.RDS")
+census_all <- census_all[!is.na(census_all$squirrel_id)]
+setnames(census_all, c("locX", "locY"), c("locx", "locy"))
+
+census_all$locx <- census_all$locx*30
+census_all$locy <- census_all$locy*30
+
+## load home made functions
+source("functions/get_spdf.R")
+source("functions/get_intersection.R")
+source("functions/get_edgelist.R")
 
 ######################################################
 ############ VALIDATION 1: CENSUS MIDDEN #############
@@ -21,67 +31,38 @@ prj <- '+init=epsg:26911'
 
 yr <- fread("output/unique-grid-years.csv")
 
-## generate list of spatial points dataframes
-out_spdf <- c()
-for(i in levels(yr$gr_year)){ 
-  
-  df2 <- census_all[gr_year == i]
-  
-  spdf <- SpatialPointsDataFrame(coordinates(cbind(df2$locX, df2$locY)),
-                                 data = df2[,c("julian","locX", "locY", "squirrel_id")],
-                                 proj4string = CRS(prj))
-  
-  out_spdf[[i]] <- st_as_sf(spdf)
-  
-}
+n = length(unique(yr$gr_year))
+
+# Generate spdf points ---------------------------------
+out_spdf <- get_spdf(df = census_all[,c("julian","locx", "locy", "squirrel_id", "gr_year")], 
+                     n = n,
+                     yr = yr)
 
 # Intersection between polygon and points ---------------------------------
-intersect_out <- c()
-for(i in 1:length(yr$gr_year)){
-  
-  intersection <- st_intersection(x = polys[[i]], y = out_spdf[[i]])
-  
-  intersect_out[[i]] <- intersection 
-}
+intersect_out <- get_intersection(poly = polys, 
+                                  spdf = out_spdf,
+                                  n = n)
 
-## name lists within intersect_out
-names(intersect_out) <- yr$gr_year
-
-edge_out <- c()
-for(i in 1:length(yr$gr_year)){ 
-  ## generate edge list with territory owners and intruders
-  edge_list <- data.table(spatial_locs = intersect_out[[i]]$id_polygons,
-                          census = intersect_out[[i]]$squirrel_id,
-                          locx = intersect_out[[i]]$locX,
-                          locy = intersect_out[[i]]$locY,
-                          julian = intersect_out[[i]]$julian)
-  
-  ## assign TRUE or FALSE value to whether a squirrel is observed on 
-  ## it's own territory (TRUE) or another territory (FALSE)
-  edge_list[, edge:= (spatial_locs==census)]
-  
-  edge_list$edge <- as.character(edge_list$edge)
-  
-  ## re-assign TRUE and FALSE values to 0s and 1s
-  edge_list$edge[edge_list$edge == "TRUE"] <- 0
-  edge_list$edge[edge_list$edge == "FALSE"] <- 1
-  
-  ## add year to list 
-  edge_list$year <- yr$gr_year[i]
-  
-  ## keep all points in file
-  edge_out[[i]] <- edge_list
-  
-}
+# Generate edge list based on polygon-point overlap ---------------------------------
+edge_out <- get_edgelist(df = intersect_out, 
+                         n = yr$gr_year)
 
 edge_list <- rbindlist(edge_out)
-edge_list[, c("year", "grid") := tstrsplit(year, "_", fixed=TRUE)]
+
+setnames(edge_list, c("owner", "intruder"), c("polygon", "census"))
+
+edge_list[, c("grid", "year") := tstrsplit(year, "_", fixed=TRUE)]
 
 
 edge_list[edge == 0][, .N, by = "year"]
 
 ## load spatial data
-df <- fread("output/spatial-locs.csv")
+df <- readRDS("output/spatial-locs.RDS")
+df <- df[gr_year != "KL_1996" & 
+           gr_year != "SU_1998" & 
+           gr_year != "KL_1999" & 
+           gr_year != "KL_2016"]
+
 
 on_territory <- df[, length(unique(squirrel_id)), by = c("year", "grid")]
 setnames(on_territory, "V1", "total_ids")
@@ -90,50 +71,8 @@ on_territory$ids_on_terr <- edge_list[edge == 0][, length(unique(census)), by = 
 ## validation of number of squirrel census locs that were on/off territories
 on_territory$propOn <- on_territory$ids_on_terr/on_territory$total_ids
 
+on_territory[grid == "KL"]
+on_territory[grid == "SU"]
 
-
-
-## merge file with edges to file with behaviours
-df_edges <- merge(life[,c("edge", "territory")], 
-                  df[], by = "territory", allow.cartesian = TRUE)
-
-## get rid of duplicate rows
-df_edges <- df_edges %>% 
-             distinct(.keep_all = TRUE)
-
-## rename behaviours 
-df$behaviour[df$behaviour == 0] <- "other"
-df$behaviour[df$behaviour == 1] <- "feeding"
-df$behaviour[df$behaviour == 2] <- "vocalizations"
-df$behaviour[df$behaviour == 3] <- "travelling"
-df$behaviour[df$behaviour == 4] <- "resting"
-df$behaviour[df$behaviour == 5] <- "in nest"
-df$behaviour[df$behaviour == 6] <- "off territory"
-df$behaviour[df$behaviour == 7] <- "interaction"
-df$behaviour[df$behaviour == 8] <- "caching"
-df$behaviour[df$behaviour == 9] <- "dead"
-df$behaviour[df$behaviour == 10] <- "grooming"
-df$behaviour[df$behaviour == 11] <- "playing"
-df$behaviour[df$behaviour == 12] <- "foraging"
-df$behaviour[df$behaviour == 13] <- "out of sight"
-df$behaviour[df$behaviour == 14] <- "nest building"
-df$behaviour[df$behaviour == 15] <- "unknown"
-df$behaviour[df$behaviour == 16] <- "scent marking"
-df$behaviour[df$behaviour == 17] <- "moving kids"
-df$behaviour[df$behaviour == 18] <- "trapped off territory"
-df$behaviour[df$behaviour == 19] <- "vigilant"
-df$behaviour[df$behaviour == 20] <- "digging for truffles"
-df$behaviour[df$behaviour == 21] <- "foot stomping"
-df$behaviour[df$behaviour == 22] <- "mating chase"
-df$behaviour[df$behaviour == "x"] <- "unknown"
-df$behaviour[df$behaviour == ""] <- "unknown"
-
-
-## summary of behaviours on own territory (0) and other squirrels teritory (1)
-sum <- df[, .N, by = c("behaviour")]
-plyr::ddply(sum, c('N'))
-
-
-ggplot(census[gr_year == "2012_KL"]) + 
-  geom_jitter(aes(locx, locy, color = squirrel_id), height = 0.2, width = 0.3) 
-  theme(legend.position = 'none')
+mean(on_territory$propOn)
+sd(on_territory$propOn)
