139d67538afff86411507e92003c2efd4ed3f800
Quinn Webber <webber.quinn@gmail.com>
2021-04-29 12:03:44 -0230

    Update FigureS2.R

--- intrusion-network-R/code/FigureS2.R
+++ intrusion-network-R/code/FigureS2.R
@@ -15,8 +15,8 @@ area_all[, c("grid", "year") := tstrsplit(gr_year, "_", fixed=TRUE)][,c("gr_year
 ## Figure
 png("figures/FigS3.png", height = 3000, width = 6000, units = "px", res = 600)
 
-ggplot(area_all, aes(as.factor(percent), y =area_ha, fill=year)) +
-  geom_point(shape = 21, alpha = 0.3, position = position_jitterdodge()) +
+aa <- ggplot(area_all, aes(as.factor(percent), y =area_ha, fill=year)) +
+  geom_jitter(shape = 21, alpha = 0.3, position = position_jitterdodge(jitter.width = 0.1)) +
   geom_boxplot(outlier.color = NA, 
                position = position_dodge2(), 
                alpha = 0.25,
@@ -27,32 +27,6 @@ ggplot(area_all, aes(as.factor(percent), y =area_ha, fill=year)) +
   ggtitle("A)") +
   xlab("Kernel density estimator percentage") +
   ylab("Territory area (ha)") +
-  theme(
-    #legend.position = c(0.15, 0.8),
-    legend.background = element_blank(),
-    legend.key = element_blank(),
-    legend.title = element_blank(),
-    axis.title.y = element_text(size = 12, color = 'black'),
-    axis.text = element_text(size = 10, color = 'black'),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    panel.border = element_rect(
-      colour = 'black',
-      fill = NA,
-      size = 1)) 
-  
-aa <- ggplot(terr_overlap_all, aes(as.factor(percent), y =area, fill=year)) +
-  geom_point(shape = 21, alpha = 0.3, position = position_jitterdodge()) +
-  geom_boxplot(outlier.color = NA, 
-               position = position_dodge2(), 
-               alpha = 0.25,
-               lwd = 0.6,
-               color = "black") +
-  scale_fill_viridis_d() +
-  scale_color_viridis_d() +
-  ggtitle("A)") +
-  xlab("Kernel density estimator percentage") +
-  ylab("Area (ha) of overlap between territories") +
   theme(
     legend.position = c(0.15, 0.8),
     legend.background = element_blank(),
@@ -66,9 +40,9 @@ aa <- ggplot(terr_overlap_all, aes(as.factor(percent), y =area, fill=year)) +
       colour = 'black',
       fill = NA,
       size = 1)) 
-
-bb <- ggplot(terr_overlap_all[percent <= 60], aes(as.factor(percent), y =area, fill=year)) +
-  geom_point(shape = 21, alpha = 0.3, position = position_jitterdodge()) +
+  
+bb <- ggplot(terr_overlap_all, aes(as.factor(percent), y =area, fill=year)) +
+  geom_jitter(shape = 21, alpha = 0.3, position = position_jitterdodge(jitter.width = 0.2)) +
   geom_boxplot(outlier.color = NA, 
                position = position_dodge2(), 
                alpha = 0.25,
@@ -79,15 +53,20 @@ bb <- ggplot(terr_overlap_all[percent <= 60], aes(as.factor(percent), y =area, f
   ggtitle("B)") +
   xlab("Kernel density estimator percentage") +
   ylab("Area (ha) of overlap between territories") +
-  theme(legend.position = 'none',
-        axis.title.y = element_text(size = 12, color = 'black'),
-        axis.text = element_text(size = 10, color = 'black'),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_rect(
-          colour = 'black',
-          fill = NA,
-          size = 1)) 
+  theme(
+    legend.position = 'none',
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    legend.title = element_blank(),
+    axis.title.y = element_text(size = 12, color = 'black'),
+    axis.text = element_text(size = 10, color = 'black'),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    panel.border = element_rect(
+      colour = 'black',
+      fill = NA,
+      size = 1)) 
+
 grid.arrange(aa,bb, nrow = 1)
 
 dev.off()
