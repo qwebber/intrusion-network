a61fad7fc775718ffee1b60acbc004ab39e19d36
Quinn Webber <webber.quinn@gmail.com>
2021-05-25 13:41:03 -0230

    Update Figure3.R

--- intrusion-network-R/code/Figure3.R
+++ intrusion-network-R/code/Figure3.R
@@ -1,6 +1,6 @@
 
 ### Packages ----
-libs <- c('data.table',
+libs <- c('data.table', 'dplyr', 'MCMCglmm',
           'ggplot2', 'gridExtra')
 lapply(libs, require, character.only = TRUE)
 
@@ -53,8 +53,16 @@ df_territory <- df_territory[!is.na(df_territory$grid)]
 
 col <- c("#f1a340", "#998ec3")
 
+## add 1998 as a dummy variable
+df_fit_strength98 <- data.table(squirrel_id = 0,
+                                grid = "KL",
+                                year = 1998,
+                                spr_density = 0,
+                                Type = "NA",
+                                Value = 0)
+
 png("figures/Fig3.png", width = 3000, height = 6000, units = "px", res = 600)
-aa <- ggplot(data = density[year >1995],
+fig3A <- ggplot(data = density[year >1995],
        aes(as.factor(year), spr_density, group = grid, color = grid)) +
   geom_point(size = 2) +
   scale_color_manual(values = col) +
@@ -65,15 +73,21 @@ aa <- ggplot(data = density[year >1995],
   scale_y_continuous(labels = scales::number_format(accuracy = 0.1),
                      limits = c(0, 4.5)) +
   scale_x_discrete(breaks = c(#1988, 1990, 1992, 1994,
-                                1996, 1998, 2000, 
-                                2002, 2004, 2006, 
-                                2008, 2010, 2012, 
-                                2014, 2016, 2018, 2020),
+                                1996, #1998, 
+                                2000, #2002, 
+                                2004, #2006, 
+                                2008, #2010, 
+                                2012, #2014,
+                                2016, #2018, 
+                                2020),
                      labels = c(#"1988","1990", "1992", "1994", 
-                                "1996", "1998", "2000", 
-                                "2002", "2004", "2006",
-                                "2008", "2010", "2012", 
-                                "2014", "2016", "2018", "2020")) +
+                                "1996", #"1998", 
+                                "2000", #"2002", 
+                                "2004", #"2006",
+                                "2008", #"2010", 
+                                "2012", #"2014", 
+                                "2016", #"2018", 
+                                "2020")) +
   geom_vline(aes(xintercept = "1993"), lty = 2) + # 1993
   geom_vline(aes(xintercept = "1998"), lty = 2) + # 1998
   geom_vline(aes(xintercept = "2005"), lty = 2) + # 2005
@@ -86,7 +100,7 @@ aa <- ggplot(data = density[year >1995],
     legend.title = element_blank(),
     legend.key = element_blank(),
     plot.title = element_text(size = 14, color = "black"),
-    axis.text.x = element_text(size = 12, color = "black", angle = 45, hjust = 1),
+    axis.text.x = element_text(size = 12, color = "black"),
     axis.text.y = element_text(size = 12, color = "black"),
     axis.title = element_text(size = 14, color = "black"),
     panel.grid.minor = element_blank(),
@@ -96,7 +110,7 @@ aa <- ggplot(data = density[year >1995],
       fill = NA,
       size = 0.5))
 
-bb <- ggplot() +
+fig3B <- ggplot() +
   geom_smooth(data = df_fit_strength, 
               aes(year, Value, group = as.factor(squirrel_id), color = grid),
               #color = "darkgrey",
@@ -104,18 +118,26 @@ bb <- ggplot() +
               method = lm,
               se = FALSE) +
   geom_vline(aes(xintercept = 3), lty = 2) + # 1998
-  geom_vline(aes(xintercept = 10), lty = 2) + # 2005
-  geom_vline(aes(xintercept = 15), lty = 2) + # 2010
-  geom_vline(aes(xintercept = 19), lty = 2) + # 2014
-  geom_vline(aes(xintercept = 24), lty = 2) + # 2019
-  scale_x_discrete(breaks = c("1996", "1998", "2000", 
-                                "2002", "2004", "2006",
-                                "2008", "2010", "2012", 
-                                "2014", "2016", "2018", "2020"),
-                     labels = c("1996", "1998", "2000", 
-                                "2002", "2004", "2006",
-                                "2008", "2010", "2012", 
-                                "2014", "2016", "2018", "2020")) +
+  geom_vline(aes(xintercept = 9), lty = 2) + # 2005
+  geom_vline(aes(xintercept = 14), lty = 2) + # 2010
+  geom_vline(aes(xintercept = 18), lty = 2) + # 2014
+  geom_vline(aes(xintercept = 23), lty = 2) + # 2019
+  scale_x_discrete(breaks = c(#1988, 1990, 1992, 1994,
+                              1996, #1998, 
+                              2000, #2002, 
+                              2004, #2006, 
+                              2008, #2010, 
+                              2012, #2014,
+                              2016, #2018, 
+                              2020),
+                              labels = c(#"1988","1990", "1992", "1994", 
+                                "1996", #"1998", 
+                                "2000", #"2002", 
+                                "2004", #"2006",
+                                "2008", #"2010", 
+                                "2012", #"2014", 
+                                "2016", #"2018", 
+                                "2020")) +
   xlab("Year") +
   ylim(-0.8, 1) +
   ylab("Intrusion strength") +
@@ -124,7 +146,7 @@ bb <- ggplot() +
   theme(
     legend.position = 'none',
     plot.title = element_text(size = 14, color = "black"),
-    axis.text.x = element_text(size = 12, color = "black", angle = 45, hjust = 1),
+    axis.text.x = element_text(size = 12, color = "black"),
     axis.text.y = element_text(size = 12, color = "black"),
     axis.title = element_text(size = 14, color = "black"),
     panel.grid.minor = element_blank(),
@@ -134,7 +156,7 @@ bb <- ggplot() +
       fill = NA,
       size = 0.5))
 
-cc <- ggplot() +
+fig3C <- ggplot() +
   geom_smooth(data = df_territory, 
               aes(year, Value, group = as.factor(squirrel_id), color = grid),
               #color = "darkgrey",
@@ -142,18 +164,26 @@ cc <- ggplot() +
               method = lm,
               se = FALSE) +
   geom_vline(aes(xintercept = 3), lty = 2) + # 1998
-  geom_vline(aes(xintercept = 10), lty = 2) + # 2005
-  geom_vline(aes(xintercept = 15), lty = 2) + # 2010
-  geom_vline(aes(xintercept = 19), lty = 2) + # 2014
-  geom_vline(aes(xintercept = 24), lty = 2) + # 2019
-  scale_x_discrete(breaks = c("1996", "1998", "2000", 
-                              "2002", "2004", "2006",
-                              "2008", "2010", "2012", 
-                              "2014", "2016", "2018", "2020"),
-                   labels = c("1996", "1998", "2000", 
-                              "2002", "2004", "2006",
-                              "2008", "2010", "2012", 
-                              "2014", "2016", "2018", "2020"))  +
+  geom_vline(aes(xintercept = 9), lty = 2) + # 2005
+  geom_vline(aes(xintercept = 14), lty = 2) + # 2010
+  geom_vline(aes(xintercept = 18), lty = 2) + # 2014
+  geom_vline(aes(xintercept = 23), lty = 2) + # 2019
+  scale_x_discrete(breaks = c(#1988, 1990, 1992, 1994,
+                              1996, #1998, 
+                              2000, #2002, 
+                              2004, #2006, 
+                              2008, #2010, 
+                              2012, #2014,
+                              2016, #2018, 
+                              2020),
+                              labels = c(#"1988","1990", "1992", "1994", 
+                                "1996", #"1998", 
+                                "2000", #"2002", 
+                                "2004", #"2006",
+                                "2008", #"2010", 
+                                "2012", #"2014", 
+                                "2016", #"2018", 
+                                "2020"))  +
   xlab("Year") +
   #ylim(-0.8, 1) +
   ylab("Territory size (ha)") +
@@ -162,7 +192,7 @@ cc <- ggplot() +
   theme(
     legend.position = 'none',
     plot.title = element_text(size = 14, color = "black"),
-    axis.text.x = element_text(size = 12, color = "black", angle = 45, hjust = 1),
+    axis.text.x = element_text(size = 12, color = "black"),
     axis.text.y = element_text(size = 12, color = "black"),
     axis.title = element_text(size = 14, color = "black"),
     panel.grid.minor = element_blank(),
@@ -171,5 +201,7 @@ cc <- ggplot() +
       colour = "black",
       fill = NA,
       size = 0.5))
-grid.arrange(aa,bb,cc, ncol = 1, nrow = 3)
+grid.arrange(fig3A,
+             fig3B,
+             fig3C, ncol = 1, nrow = 3)
 dev.off()
