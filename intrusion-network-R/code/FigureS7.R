9b4790271fddcb3a8f7d7531a83adbd7ef067631
Quinn Webber <webber.quinn@gmail.com>
2021-06-07 15:29:01 -0230

    Update FigureS7.R

--- intrusion-network-R/code/FigureS7.R
+++ intrusion-network-R/code/FigureS7.R
@@ -8,8 +8,35 @@ lapply(libs, require, character.only = TRUE)
 edge_list <- readRDS("output/edge-list-true.RDS")
 edge_list$gr_year <- as.factor(paste(edge_list$year, edge_list$grid, sep = "_"))
 
-ggplot(edge_list[, .N, by = c("julian", "year", "grid", "gr_year")],
+col <- c("#f1a340", "#998ec3")
+
+setnames(edge_list, "grid", "Grid")
+
+png("figures/FigS7.png", height = 6000, width = 6000, units = "px", res = 600)
+ggplot(edge_list[edge == 1][, .N, by = c("julian", "year", "Grid", "gr_year")],
        aes(julian, N)) +
-  geom_point(aes(color = grid)) +
-  geom_smooth(method = "loess") +
-  facet_wrap(~year, scale = "free")
+  geom_point(aes(color = Grid),
+             alpha = 0.5) +
+  geom_smooth(method = "loess", color = "black") +
+  scale_color_manual(values = col) +
+  facet_wrap(~year, scale = "free") +
+  xlim(70, 250) +
+  ylab("Frequency of intrusions") +
+  xlab("Day of the year") +
+  theme(
+    legend.key = element_blank(),
+    legend.text = element_text(size = 12, color = "black"),
+    legend.title = element_text(size = 14, color = "black"), 
+    plot.title = element_text(size = 14, color = "black"),
+    axis.text.x = element_text(size = 12, color = "black", hjust = 1),
+    axis.text.y = element_text(size = 12, color = "black"),
+    axis.title = element_text(size = 14, color = "black"),
+    strip.background = element_rect(fill = NA, color = "black"),
+    strip.text = element_text(size = 12, color = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    panel.border = element_rect(
+      colour = "black",
+      fill = NA,
+      size = 0.5))
+dev.off()
