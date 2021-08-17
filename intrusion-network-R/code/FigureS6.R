7afd60e5c4fb537f485e4ded0b7c5af8312f496c
Quinn Webber <webber.quinn@gmail.com>
2021-08-17 13:34:32 -0230

    Update FigureS6.R

--- intrusion-network-R/code/FigureS6.R
+++ intrusion-network-R/code/FigureS6.R
@@ -1,62 +1,132 @@
 
 
-library(data.table)
-library(ggplot2)
 
-df <- readRDS("output/spatial-locs.RDS")
-df$squirrel_id <- as.character(df$squirrel_id)
-df$gr_year <- as.character(df$gr_year)
 
-polys <- readRDS("output/edge-list-inputs/polygons-all.RDS")
-KL_2005 <- sf::st_transform(polys$KL_2005)
-KL_2005$gr_year <- rep("2005", length(KL_2005$id_polygons))
+### Packages ----
+libs <- c('data.table',
+          'ggplot2')
+lapply(libs, require, character.only = TRUE)
 
-KL_2006 <- sf::st_transform(polys$KL_2006)
-KL_2006$gr_year <- rep("2006", length(KL_2006$id_polygons))
+### load data 
+all <- readRDS("output/final-df.RDS")
+all$squirrel_id <- as.factor(all$squirrel_id)
+all$year <- as.factor(all$year)
 
-## subset point data
-sq <- df[squirrel_id == 7390 |
-           squirrel_id == 7106 |
-           squirrel_id == 8727 |
-           squirrel_id == 6868 |
-           squirrel_id == 10138] 
+unique(all$gr_year)
 
-## subset polygons
-polys05 <- subset(KL_2005, id_polygons == 7390 |
-                    id_polygons == 7106 |
-                    id_polygons == 8727 |
-                    id_polygons == 6868 |
-                    id_polygons == 10138)
-polys06 <- subset(KL_2006, id_polygons == 7390 |
-                    id_polygons == 7106 |
-                    id_polygons == 8727 |
-                    id_polygons == 6868 |
-                    id_polygons == 10138)
+all$area_ha <- all$area_m2/10000
 
-polys_all <- rbind(polys05, polys06)
-polys_all$year <- polys_all$gr_year
+all <- all[!is.na(all$sex)]
+all <- all[!is.na(all$grid)]
+
+## Territory size
+median(all[gr_year != "KL_2006"]$area_ha)
+sd(all[gr_year != "KL_2006"]$area_ha)
+median(all[gr_year == "KL_2006"]$area_ha)
+sd(all[gr_year == "KL_2006"]$area_ha)
+
+
+## Out-strength
+median(all[gr_year != "KL_2006"]$outstrength)
+sd(all[gr_year != "KL_2006"]$outstrength)
+median(all[gr_year == "KL_2006"]$outstrength)
+sd(all[gr_year == "KL_2006"]$outstrength)
+
+## In-strength
+median(all[gr_year != "KL_2006"]$instrength)
+sd(all[gr_year != "KL_2006"]$instrength)
+median(all[gr_year == "KL_2006"]$instrength)
+sd(all[gr_year == "KL_2006"]$instrength)
+
 
 png("figures/FigS6.png", width = 6000, height = 3000, units = "px", res = 600)
-ggplot(data = polys_all) +
-  geom_sf(aes(fill = id_polygons), 
-          alpha = 0.5) +
-  coord_sf(datum = st_crs(32648)) +
-  geom_jitter(data = sq[year == "2005" | 
-                        year == "2006"], aes(locx, locy, color = factor(squirrel_id)), 
-              alpha = 0.5) + 
-  ylab("") + xlab("") +
-  scale_color_viridis_d() +
-  scale_fill_viridis_d() +
-  theme(#legend.position = 'none',
-        legend.key = element_blank(),
-        axis.text=element_blank(),
-        axis.title=element_blank(),
-        axis.ticks = element_blank(),
-        strip.text = element_text(size=12),
-        strip.background = element_rect(fill = "white", color = "black", size = 1),
-        panel.grid.major = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_rect(colour = "black", fill=NA, size=1)) + 
-  facet_wrap(~year)
-dev.off()
+FigS6a <- ggplot() +
+  geom_density(data = all[gr_year == "KL_2006"], aes(area_ha), 
+               fill = "red", 
+               alpha = 0.5) +
+  geom_vline(data = all[gr_year == "KL_2006"], 
+             aes(xintercept = median(area_ha)), 
+             lty = 2) +
+  geom_density(data = all[gr_year != "KL_2006"], aes(area_ha), 
+               fill = "blue", 
+               alpha = 0.5) +
+  geom_vline(data = all[gr_year != "KL_2006"], 
+             aes(xintercept = median(area_ha)), 
+             lty = 2) +
+  ylab("Density distribution") +
+  xlab("Territory size (ha)") +
+  ggtitle("A)") + 
+  theme(
+    legend.position = 'none',
+    plot.title = element_text(size = 14, color = "black"),
+    axis.text.x = element_text(size = 12, color = "black"),
+    axis.text.y = element_text(size = 12, color = "black"),
+    axis.title = element_text(size = 14, color = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    panel.border = element_rect(
+      colour = "black",
+      fill = NA,
+      size = 1)) 
 
+FigS6b <- ggplot() +
+  geom_density(data = all[gr_year == "KL_2006"], aes(outstrength), 
+               fill = "red", 
+               alpha = 0.5) +
+  geom_vline(data = all[gr_year == "KL_2006"], 
+             aes(xintercept = median(outstrength)), 
+             lty = 2) +
+  geom_density(data = all[gr_year != "KL_2006"], aes(outstrength), 
+               fill = "blue", 
+               alpha = 0.5) +
+  geom_vline(data = all[gr_year != "KL_2006"], 
+             aes(xintercept = median(outstrength)), 
+             lty = 2) +
+  ylab("Density distribution") +
+  xlab("Out-intrusion-strength") +
+  ggtitle("B)") +
+  theme(
+    legend.position = 'none',
+    plot.title = element_text(size = 14, color = "black"),
+    axis.text.x = element_text(size = 12, color = "black"),
+    axis.text.y = element_text(size = 12, color = "black"),
+    axis.title = element_text(size = 14, color = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    panel.border = element_rect(
+      colour = "black",
+      fill = NA,
+      size = 1)) 
+
+FigS6c <- ggplot() +
+  geom_density(data = all[gr_year == "KL_2006"], aes(instrength), 
+               fill = "red", 
+               alpha = 0.5) +
+  geom_vline(data = all[gr_year == "KL_2006"], 
+             aes(xintercept = median(instrength)), 
+             lty = 2) +
+  geom_density(data = all[gr_year != "KL_2006"], aes(instrength), 
+               fill = "blue", 
+               alpha = 0.5) +
+  geom_vline(data = all[gr_year != "KL_2006"], 
+             aes(xintercept = median(instrength)), 
+             lty = 2) +
+  ylab("Density distribution") +
+  xlab("In-intrusion-strength") +
+  ggtitle("C)") +
+  xlim(0,1.1) +
+  theme(
+    legend.position = 'none',
+    plot.title = element_text(size = 14, color = "black"),
+    axis.text.x = element_text(size = 12, color = "black"),
+    axis.text.y = element_text(size = 12, color = "black"),
+    axis.title = element_text(size = 14, color = "black"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    panel.border = element_rect(
+      colour = "black",
+      fill = NA,
+      size = 1)) 
+
+grid.arrange(FigS6a, FigS6b, FigS6c,nrow = 1)
+dev.off()
