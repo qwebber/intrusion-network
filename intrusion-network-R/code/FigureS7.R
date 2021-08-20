fd234a3ae845eb5ef236a490025527e454d8dc52
Quinn Webber <webber.quinn@gmail.com>
2021-08-20 14:54:14 -0230

    Update FigureS7.R

--- intrusion-network-R/code/FigureS7.R
+++ intrusion-network-R/code/FigureS7.R
@@ -2,131 +2,35 @@
 
 
 
-### Packages ----
-libs <- c('data.table',
-          'ggplot2')
-lapply(libs, require, character.only = TRUE)
+## load packages
+library(ggplot2)
+library(gridExtra)
+library(data.table)
+library(glmmTMB)
 
-### load data 
-all <- readRDS("output/final-df.RDS")
-all$squirrel_id <- as.factor(all$squirrel_id)
-all$year <- as.factor(all$year)
+## load data
+terr_overlap_all <- readRDS("output/territories/territory-overlap.RDS")
+terr_overlap_all <- terr_overlap_all[percent == 50]
 
-unique(all$gr_year)
-
-all$area_ha <- all$area_m2/10000
-
-all <- all[!is.na(all$sex)]
-all <- all[!is.na(all$grid)]
-
-## Territory size
-median(all[gr_year != "KL_2006"]$area_ha)
-sd(all[gr_year != "KL_2006"]$area_ha)
-median(all[gr_year == "KL_2006"]$area_ha)
-sd(all[gr_year == "KL_2006"]$area_ha)
-
-
-## Out-strength
-median(all[gr_year != "KL_2006"]$outstrength)
-sd(all[gr_year != "KL_2006"]$outstrength)
-median(all[gr_year == "KL_2006"]$outstrength)
-sd(all[gr_year == "KL_2006"]$outstrength)
-
-## In-strength
-median(all[gr_year != "KL_2006"]$instrength)
-sd(all[gr_year != "KL_2006"]$instrength)
-median(all[gr_year == "KL_2006"]$instrength)
-sd(all[gr_year == "KL_2006"]$instrength)
-
-
-png("figures/FigS6.png", width = 6000, height = 3000, units = "px", res = 600)
-FigS6a <- ggplot() +
-  geom_density(data = all[gr_year == "KL_2006"], aes(area_ha), 
-               fill = "red", 
-               alpha = 0.5) +
-  geom_vline(data = all[gr_year == "KL_2006"], 
-             aes(xintercept = median(area_ha)), 
-             lty = 2) +
-  geom_density(data = all[gr_year != "KL_2006"], aes(area_ha), 
-               fill = "blue", 
-               alpha = 0.5) +
-  geom_vline(data = all[gr_year != "KL_2006"], 
-             aes(xintercept = median(area_ha)), 
-             lty = 2) +
-  ylab("Density distribution") +
-  xlab("Territory size (ha)") +
-  ggtitle("A)") + 
+png("figures/FigS7.png", height = 3000, width = 3000, units = "px", res = 600)
+ggplot(terr_overlap_all, aes(as.factor(year), y =area, fill=year)) +
+  geom_jitter(shape = 21, alpha = 0.3, 
+              position = position_jitterdodge(jitter.width = 3)) +
+  scale_fill_viridis_d() +
+  scale_color_viridis_d() +
+  xlab("Year") +
+  ylab("Area (ha) of overlap between territories") +
   theme(
     legend.position = 'none',
-    plot.title = element_text(size = 14, color = "black"),
-    axis.text.x = element_text(size = 12, color = "black"),
-    axis.text.y = element_text(size = 12, color = "black"),
-    axis.title = element_text(size = 14, color = "black"),
+    legend.background = element_blank(),
+    legend.key = element_blank(),
+    legend.title = element_blank(),
+    axis.title.y = element_text(size = 12, color = 'black'),
+    axis.text = element_text(size = 10, color = 'black'),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
     panel.border = element_rect(
-      colour = "black",
+      colour = 'black',
       fill = NA,
       size = 1)) 
-
-FigS6b <- ggplot() +
-  geom_density(data = all[gr_year == "KL_2006"], aes(outstrength), 
-               fill = "red", 
-               alpha = 0.5) +
-  geom_vline(data = all[gr_year == "KL_2006"], 
-             aes(xintercept = median(outstrength)), 
-             lty = 2) +
-  geom_density(data = all[gr_year != "KL_2006"], aes(outstrength), 
-               fill = "blue", 
-               alpha = 0.5) +
-  geom_vline(data = all[gr_year != "KL_2006"], 
-             aes(xintercept = median(outstrength)), 
-             lty = 2) +
-  ylab("Density distribution") +
-  xlab("Out-intrusion-strength") +
-  ggtitle("B)") +
-  theme(
-    legend.position = 'none',
-    plot.title = element_text(size = 14, color = "black"),
-    axis.text.x = element_text(size = 12, color = "black"),
-    axis.text.y = element_text(size = 12, color = "black"),
-    axis.title = element_text(size = 14, color = "black"),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    panel.border = element_rect(
-      colour = "black",
-      fill = NA,
-      size = 1)) 
-
-FigS6c <- ggplot() +
-  geom_density(data = all[gr_year == "KL_2006"], aes(instrength), 
-               fill = "red", 
-               alpha = 0.5) +
-  geom_vline(data = all[gr_year == "KL_2006"], 
-             aes(xintercept = median(instrength)), 
-             lty = 2) +
-  geom_density(data = all[gr_year != "KL_2006"], aes(instrength), 
-               fill = "blue", 
-               alpha = 0.5) +
-  geom_vline(data = all[gr_year != "KL_2006"], 
-             aes(xintercept = median(instrength)), 
-             lty = 2) +
-  ylab("Density distribution") +
-  xlab("In-intrusion-strength") +
-  ggtitle("C)") +
-  xlim(0,1.1) +
-  theme(
-    legend.position = 'none',
-    plot.title = element_text(size = 14, color = "black"),
-    axis.text.x = element_text(size = 12, color = "black"),
-    axis.text.y = element_text(size = 12, color = "black"),
-    axis.title = element_text(size = 14, color = "black"),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    panel.border = element_rect(
-      colour = "black",
-      fill = NA,
-      size = 1)) 
-
-grid.arrange(FigS6a, FigS6b, FigS6c,nrow = 1)
 dev.off()
