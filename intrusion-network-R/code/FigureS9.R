5b2c343db01148732337b90228ba74a051f1f595
Quinn Webber <webber.quinn@gmail.com>
2021-09-30 10:09:49 -0230

    Update FigureS9.R

--- intrusion-network-R/code/FigureS9.R
+++ intrusion-network-R/code/FigureS9.R
@@ -1,12 +1,19 @@
 
+
+
 ### Packages ----
-libs <- c('data.table','dplyr',
-          'ggplot2', 'gridExtra', 
-          'MCMCglmm')
+libs <- c('data.table', 'dplyr', 'MCMCglmm',
+          'ggplot2', 'gridExtra')
 lapply(libs, require, character.only = TRUE)
 
 ## load data
+density <- readRDS("output/auxilliary-data/spring-density.RDS")
+#density <- setDT(density)[year > 1995] 
+density <- setDT(density)[grid == "KL" | grid == "SU"]
+
+## load data
 all <- readRDS("output/final-df.RDS")
+all <- all[gr_year != "KL_2006"]
 all$squirrel_id <- as.factor(all$squirrel_id)
 all$year <- as.factor(all$year)
 
@@ -17,67 +24,73 @@ all$area_ha <- all$area_m2/10000
 all <- all[!is.na(all$sex)]
 all <- all[!is.na(all$grid)]
 
-## scale variables within year:
-all[, outstrengthScale := scale(outstrength), by = c("year", "grid")]
-all[, instrengthScale := scale(instrength), by = c("year", "grid")]
-all[, areaScale := scale(area_ha), by = c("year", "grid")]
+all$sex[all$sex == "F"] <- "Female"
+all$sex[all$sex == "M"] <- "Male"
+
+
+## scale variables:
+all[, outstrengthScale := scale(outstrength)]
+all[, instrengthScale := scale(instrength)]
+all[, areaScale := scale(area_ha)]
 all[, densityScale := scale(spr_density)]
 
 ## load models
 mcmc_strength <- readRDS("output/models/mcmc_strength.RDS")
 mcmc_territory <- readRDS("output/models/mcmc_territory.RDS")
 mcmc_in <- readRDS("output/models/mcmc_instrength.RDS")
+mcmc_strength_behav <- readRDS("output/models/mcmc_outstrength_behav.RDS")
+mcmc_instrength_behav <- readRDS("output/models/mcmc_instrength_behav.RDS")
+
 
 ## convert to BRN format
 df_strength <- cbind(all,
                      fit = predict(mcmc_strength, marginal = NULL)) %>%
-  group_by(squirrel_id, grid, year, spr_density) %>%
+  group_by(squirrel_id, grid, year, densityScale, spr_density, sex) %>%
   dplyr::summarise(fit = mean(fit.V1),
                    outstrengthScale = mean(outstrengthScale)) %>%
   tidyr::gather(Type, Value,
                 fit:outstrengthScale)
 
-df_fit_strength = setDT(df_strength)[Type == "fit"]
-df_fit_strength <- df_fit_strength[!is.na(df_fit_strength$grid)]
+df_strength = subset(df_strength, Type == "fit")
 
 ## Territory size
 df_territory <- cbind(all,
-                     fit = predict(mcmc_territory, marginal = NULL)) %>%
-  group_by(squirrel_id, grid, year, spr_density) %>%
+                      fit = predict(mcmc_territory, marginal = NULL)) %>%
+  group_by(squirrel_id, grid, year, densityScale, spr_density, sex) %>%
   dplyr::summarise(fit = mean(fit.V1),
                    areaScale = mean(areaScale)) %>%
   tidyr::gather(Type, Value,
                 fit:areaScale)
 
-df_territory = setDT(df_territory)[Type == "fit"]
-df_territory <- df_territory[!is.na(df_territory$grid)]
+df_territory = subset(df_territory, Type == "fit")
 
 ## In-strength
 df_in <- cbind(all,
-                      fit = predict(mcmc_in, marginal = NULL)) %>%
-  group_by(squirrel_id, grid, year, spr_density) %>%
+               fit = predict(mcmc_in, marginal = NULL)) %>%
+  group_by(squirrel_id, grid, year, densityScale, spr_density, sex) %>%
   dplyr::summarise(fit = mean(fit.V1),
                    instrengthScale = mean(instrengthScale)) %>%
   tidyr::gather(Type, Value,
                 fit:instrengthScale)
 
-df_in = setDT(df_in)[Type == "fit"]
-df_in <- df_in[!is.na(df_in$grid)]
-
+df_in = subset(df_in, Type == "fit")
 
 col <- c("#f1a340", "#998ec3")
 
-#png("figures/FigS8.png", height = 6000, width = 8000, units = "px", res = 600)
-Fig4A <- ggplot(data = df_fit_strength) +
-  geom_jitter(aes(spr_density, Value, group = as.factor(squirrel_id), color = grid),
-              alpha = 0.5) +
+png("figures/FigS9.png", width = 4000, height = 8000, units = "px", res = 600)
+FigS9A <- ggplot(data = df_strength) +
+  geom_smooth(aes(spr_density, Value, group = as.factor(squirrel_id), color = grid),
+              #color = "darkgrey",
+              size = 0.25,
+              alpha = 0.5,
+              method = lm,
+              se = FALSE) +
   geom_smooth(aes(spr_density, Value), 
               method = lm, 
               color = "black") +
   scale_color_manual(values = col) +
-  ylim(-0.8, 1) +
   xlab("Spring density (squirrels/ha)") +
-  ylab("Intrusion out-strength") +
+  ylab("Out-intrusion-strength") +
   ggtitle('A)') +
   theme(
     legend.position = 'none',
@@ -85,48 +98,60 @@ Fig4A <- ggplot(data = df_fit_strength) +
     axis.text.x = element_text(size = 12, color = "black", hjust = 1),
     axis.text.y = element_text(size = 12, color = "black"),
     axis.title = element_text(size = 14, color = "black"),
+    strip.background = element_rect(fill = "white", color = "black"),
+    strip.text = element_text(size = 14, color = "black"),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
     panel.border = element_rect(
       colour = "black",
       fill = NA,
-      size = 0.5))
+      size = 0.5)) +
+  facet_wrap(~sex)
 
-
-Fig4B <- ggplot(data = df_territory) +
-  geom_jitter(aes(spr_density, Value, group = as.factor(squirrel_id), color = grid),
-              alpha = 0.5) +
+FigS9B <- ggplot(data = df_territory) +
+  geom_smooth(aes(spr_density, Value, group = as.factor(squirrel_id), color = grid),
+              #color = "darkgrey",
+              size = 0.25,
+              alpha = 0.5,
+              method = lm,
+              se = FALSE) +
   geom_smooth(aes(spr_density, Value), 
               method = lm, 
               color = "black") +
   scale_color_manual(values = col) +
   #ylim(-0.8, 1) +
   xlab("Spring density (squirrels/ha)") +
-  ylab("Territory size (ha)") +
+  ylab("Territory size") +
   ggtitle('B)') +
-  theme(
-    legend.position = 'none',
-    plot.title = element_text(size = 14, color = "black"),
-    axis.text.x = element_text(size = 12, color = "black", hjust = 1),
-    axis.text.y = element_text(size = 12, color = "black"),
-    axis.title = element_text(size = 14, color = "black"),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    panel.border = element_rect(
-      colour = "black",
-      fill = NA,
-      size = 0.5)) 
+  theme(legend.position = 'none',
+        plot.title = element_text(size = 14, color = "black"),
+        axis.text.x = element_text(size = 12, color = "black"),
+        axis.text.y = element_text(size = 12, color = "black"),
+        axis.title = element_text(size = 14, color = "black"),
+        strip.background = element_rect(fill = "white", color = "black"),
+        strip.text = element_text(size = 14, color = "black"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(
+          colour = "black",
+          fill = NA,
+          size = 0.5)) +
+  facet_wrap(~sex)
 
-Fig4C <- ggplot(data = df_in) +
-  geom_jitter(aes(spr_density, Value, group = as.factor(squirrel_id), color = grid),
-              alpha = 0.5) +
+FigS9C <- ggplot(data = df_in) +
+  geom_smooth(aes(spr_density, Value, group = as.factor(squirrel_id), color = grid),
+              #color = "darkgrey",
+              size = 0.25,
+              alpha = 0.5,
+              method = lm,
+              se = FALSE) +
   geom_smooth(aes(spr_density, Value), 
               method = lm, 
               color = "black") +
   scale_color_manual(values = col) +
   scale_y_continuous(labels = scales::number_format(accuracy = 0.1)) +
   xlab("Spring density (squirrels/ha)") +
-  ylab("Intrusion in-strength") +
+  ylab("In-intrusion-strength") +
   ggtitle('C)') +
   theme(
     legend.position = 'none',
@@ -134,100 +159,15 @@ Fig4C <- ggplot(data = df_in) +
     axis.text.x = element_text(size = 12, color = "black", hjust = 1),
     axis.text.y = element_text(size = 12, color = "black"),
     axis.title = element_text(size = 14, color = "black"),
+    strip.background = element_rect(fill = "white", color = "black"),
+    strip.text = element_text(size = 14, color = "black"),
     panel.grid.minor = element_blank(),
     panel.background = element_blank(),
     panel.border = element_rect(
       colour = "black",
       fill = NA,
-      size = 0.5)) 
+      size = 0.5)) +
+  facet_wrap(~sex)
 
-
-
-Fig4D <- ggplot(data = df_fit_strength) +
-  geom_smooth(aes(spr_density, Value, group = as.factor(squirrel_id), color = grid),
-              #color = "darkgrey",
-              size = 0.25,
-              alpha = 0.5,
-              method = lm,
-              se = FALSE) +
-  geom_smooth(aes(spr_density, Value), 
-              method = lm, 
-              color = "black") +
-  scale_color_manual(values = col) +
-  ylim(-0.8, 1) +
-  xlab("Spring density (squirrels/ha)") +
-  ylab("Intrusion out-strength") +
-  ggtitle('D)') +
-  theme(
-    legend.position = 'none',
-    plot.title = element_text(size = 14, color = "black"),
-    axis.text.x = element_text(size = 12, color = "black", hjust = 1),
-    axis.text.y = element_text(size = 12, color = "black"),
-    axis.title = element_text(size = 14, color = "black"),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    panel.border = element_rect(
-      colour = "black",
-      fill = NA,
-      size = 0.5))
-
-Fig4E <- ggplot(data = df_territory ) +
-  geom_smooth(aes(spr_density, Value, group = as.factor(squirrel_id), color = grid),
-              #color = "darkgrey",
-              size = 0.25,
-              alpha = 0.5,
-              method = lm,
-              se = FALSE) +
-  geom_smooth(aes(spr_density, Value), 
-              method = lm, 
-              color = "black") +
-  scale_color_manual(values = col) +
-  #ylim(-0.8, 1) +
-  xlab("Spring density (squirrels/ha)") +
-  ylab("Territory size (ha)") +
-  ggtitle('E)') +
-  theme(
-    legend.position = 'none',
-    plot.title = element_text(size = 14, color = "black"),
-    axis.text.x = element_text(size = 12, color = "black", hjust = 1),
-    axis.text.y = element_text(size = 12, color = "black"),
-    axis.title = element_text(size = 14, color = "black"),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    panel.border = element_rect(
-      colour = "black",
-      fill = NA,
-      size = 0.5)) 
-
-Fig4F <- ggplot(data = df_in) +
-  geom_smooth(aes(spr_density, Value, group = as.factor(squirrel_id), color = grid),
-              #color = "darkgrey",
-              size = 0.25,
-              alpha = 0.5,
-              method = lm,
-              se = FALSE) +
-  geom_smooth(aes(spr_density, Value), 
-              method = lm, 
-              color = "black") +
-  scale_color_manual(values = col) +
-  scale_y_continuous(labels = scales::number_format(accuracy = 0.1)) +
-  xlab("Spring density (squirrels/ha)") +
-  ylab("Intrusion in-strength") +
-  ggtitle('F)') +
-  theme(
-    legend.position = 'none',
-    plot.title = element_text(size = 14, color = "black"),
-    axis.text.x = element_text(size = 12, color = "black", hjust = 1),
-    axis.text.y = element_text(size = 12, color = "black"),
-    axis.title = element_text(size = 14, color = "black"),
-    panel.grid.minor = element_blank(),
-    panel.background = element_blank(),
-    panel.border = element_rect(
-      colour = "black",
-      fill = NA,
-      size = 0.5)) 
-
-
-grid.arrange(Fig4A, Fig4B, Fig4C, 
-             Fig4D, Fig4E, Fig4F, nrow = 2)
-#dev.off()
+grid.arrange(FigS9A, FigS9B, FigS9C, nrow = 3)
+dev.off()
