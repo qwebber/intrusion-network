482adde0349c9c07eeacbdec278aaa08962fa7cc
Quinn Webber <webber.quinn@gmail.com>
2021-09-30 10:08:08 -0230

    Update 05-generate-networks.R

--- intrusion-network-R/code/05-generate-networks.R
+++ intrusion-network-R/code/05-generate-networks.R
@@ -33,7 +33,7 @@ for(i in 1:n){
   ## generate territoriality index (intrusions/intrusions + total locs)
   df2 <- data.table(owner = df1$owner,
                     intruder = df1$intruder, 
-                    TI = df1$N/(df1$N + df1$Nintruder))
+                    TI = df1$N)#/(df1$N + df1$Nintruder))
   
   adj <- AdjacencyFromEdgelist(df2)
   
@@ -53,9 +53,14 @@ metrics2[, c("year", "grid") := tstrsplit(gr_year, "_", fixed=TRUE)]#[,c("gr_yea
 metrics2$id_gr_yr <- as.factor(paste(metrics2$squirrel_id, metrics2$gr_year, sep = "_"))
 
 
+
 ## generate metrics only using behavioural data
 metrics_behav <- c()
 edge_behav <- edge_list[data == "behaviour"]
+edge_behav[, Nowner := .N, by = c("owner", "grid", "year", "data")]
+## number of obs per intruder for behavioural data only
+edge_behav[, Nintruder := .N, by = c("intruder", "grid", "year", "data")]
+
 for(i in 1:n){ 
   
   k <- gr_year[i]
@@ -65,7 +70,7 @@ for(i in 1:n){
   ## generate territoriality index (intrusions/intrusions + total locs)
   df2 <- data.table(owner = df1$owner,
                     intruder = df1$intruder, 
-                    TI = df1$N/(df1$N + df1$Nintruder))
+                    TI = df1$N) #/(df1$N + df1$Nintruder))
   
   adj <- AdjacencyFromEdgelist(df2)
   
@@ -84,8 +89,72 @@ metrics_behav2 <- rbindlist(metrics_behav, fill = T)
 metrics_behav2[, c("year", "grid") := tstrsplit(gr_year, "_", fixed=TRUE)]#[,c("gr_year") := NULL]
 metrics_behav2$id_gr_yr <- as.factor(paste(metrics_behav2$squirrel_id, metrics_behav2$gr_year, sep = "_"))
 
+
+## generate metrics only using trapping data
+metrics_trap <- c()
+edge_trap <- edge_list[data == "trap"]
+edge_trap[, Nowner := .N, by = c("owner", "grid", "year", "data")]
+## number of obs per intruder for trapping data only
+edge_trap[, Nintruder := .N, by = c("intruder", "grid", "year", "data")]
+
+for(i in 1:n){ 
+  
+  k <- gr_year[i]
+  
+  df1 <- edge_trap[gr_year == k][, .N, by = c("owner", "intruder", "Nintruder")]
+  
+  ## generate territoriality index (intrusions/intrusions + total locs)
+  df2 <- data.table(owner = df1$owner,
+                    intruder = df1$intruder, 
+                    TI = df1$N)#/(df1$N + df1$Nintruder))
+  
+  adj <- AdjacencyFromEdgelist(df2)
+  
+  diag(adj$adjacency) <- 0
+  
+  g <- graph_from_adjacency_matrix(adj$adjacency, weighted = TRUE, mode = c("directed")) 
+  
+  metrics_trap[[i]] <- data.table(outstrength_trap = graph.strength(g, mode = c("out")),
+                                   instrength_trap = graph.strength(g, mode = c("in")),
+                                   squirrel_id = adj$nodelist,
+                                   gr_year = k)
+  
+}
+
+metrics_trap2 <- rbindlist(metrics_trap, fill = T)
+metrics_trap2[, c("year", "grid") := tstrsplit(gr_year, "_", fixed=TRUE)]#[,c("gr_year") := NULL]
+metrics_trap2$id_gr_yr <- as.factor(paste(metrics_trap2$squirrel_id, metrics_trap2$gr_year, sep = "_"))
+
+
 metrics_all <- merge(metrics2, metrics_behav2[,c("outstrength_behav", "instrength_behav", "id_gr_yr")], 
                      by = "id_gr_yr", all = T)
 
-saveRDS(metrics2, "output/metrics.RDS")
+metrics_all2 <- merge(metrics_all, metrics_trap2[,c("outstrength_trap", "instrength_trap", "id_gr_yr")], 
+                      by = "id_gr_yr", all = T)
+
+fake <- data.table(x = 0:1000,
+                   y = 0:1000)
+
+fake2 <- data.table(x = 0:5500,
+                   y = 0:5500)
+
+aa <- ggplot(metrics_all2) + 
+  geom_point(aes(outstrength_trap, outstrength)) +
+  geom_line(data = fake, aes(x,y), color = "red") +
+  ggtitle("RED LINE IS 1:1 line") +
+  ylab("Outstrength (all data)") +
+  xlab("Outstrength (trapping data)") 
+bb <- ggplot(metrics_all2) + 
+  geom_point(aes(outstrength_trap, outstrength_behav)) +
+  geom_line(data = fake, aes(x,y), color = "red") +
+  xlab("Outstrength (trapping data)") +
+  ylab("Outstrength (behavioural obs data)") 
+cc <- ggplot(metrics_all2) + 
+  geom_point(aes(outstrength, outstrength_behav)) +
+  geom_line(data = fake2, aes(x,y), color = "red") +
+  ylab("Outstrength (behavioural obs data)") +
+  xlab("Outstrength (all data)") 
+grid.arrange(aa,bb,cc, nrow = 1)
+
+saveRDS(metrics_all2, "output/metrics.RDS")
 
