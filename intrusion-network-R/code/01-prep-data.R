338e8161a2366315c00d4261befceeec4f382b3e
Quinn Webber <webber.quinn@gmail.com>
2021-11-01 09:57:20 -0230

    Update 01-prep-data.R

--- intrusion-network-R/code/01-prep-data.R
+++ intrusion-network-R/code/01-prep-data.R
@@ -117,44 +117,67 @@ df$gr_year <- as.factor(paste(df$grid, df$year, sep = "_"))
 ## drop individuals with fewer than 20 obs per year
 df2 <- df[N > 21]
 
-## drop instances where >10 observatioHns in a day
+## assing observation number per id/day/grid/year
 df2[, rowDay := seq_len(.N), by = c("squirrel_id","julian","grid", "year")]
+df2$id_gr_yr_day <- as.factor(paste(df2$squirrel_id, df2$grid,
+                                    df2$year, df2$julian, df2$rowDay, sep = "_"))
 
-## remove any observations more than 30 in a day
-df2 <- df2[rowDay < 31]
+
+
+## split to assign rows to either be IDs with > 30 or <30 locs per day
+under30 <- df2[, if (.N < 31) .SD, by = c("squirrel_id","julian","grid", "year")]
+over30 <- df2[, if (.N > 30) .SD, by = c("squirrel_id","julian","grid", "year")]
+over30$rowDay2 <- "over"
+over30$id_gr_yr_day <- as.factor(paste(over30$squirrel_id, over30$grid,
+                                       over30$year, over30$julian, over30$rowDay, sep = "_"))
+
+## randomly select 30 locs per id/day/grid/year
+over30_2 <- over30[, sample(rowDay, 30), by = c("squirrel_id","julian","grid", "year")]
+setnames(over30_2,  "V1", "rowDay")
+over30_2$id_gr_yr_day <- as.factor(paste(over30_2$squirrel_id, over30_2$grid,
+                                         over30_2$year, over30_2$julian, over30_2$rowDay, sep = "_"))
+
+## merge data from original DT back with >30 DT subset
+over30_3 <- merge(df2, 
+                  over30_2[, c("squirrel_id","julian","grid", "year", "rowDay") := NULL], 
+                  by = "id_gr_yr_day")
+
+
+## merge DT < 30 locs/day with DT > randomly sampled 30 locs/day 
+df3 <- rbind(under30, over30_3)
 
 ## check number of IDs per grid-year
-df2[, NID := uniqueN(squirrel_id), by = c("grid", "year")]
+df3[, NID := uniqueN(squirrel_id), by = c("grid", "year")]
 
 ## check unique squirrels per year
-df2[, uniqueN(squirrel_id), by = c("grid", "year")]
+df3[, uniqueN(squirrel_id), by = c("grid", "year")]
 
 ## remove grid-years with <10 unique squirrels
-df2 <- df2[NID > 11]
+df3 <- df3[NID > 11]
 
 ## number of trappingevents/behavioural observations
-df2[, .N, by = c("data")]
+df3[, .N, by = c("data")]
 
 ## order dataframe by grid and year
-df2 <- setDT(ddply(df2, c('year', 'grid')))
+df3 <- setDT(ddply(df3, c('year', 'grid')))
 
 ## convert locx and locy to metres
-df2$locx <- df2$locx*30
-df2$locy <- df2$locy*30
+df3$locx <- df3$locx*30
+df3$locy <- df3$locy*30
 
 #### assign a jitter to all locx and locy
 
 ## for trappign data use 15m jitter
-dfTrap <- df2[data == "trap"]
+dfTrap <- df3[data == "trap"]
 dfTrap$locx <- dfTrap$locx + sample(1:15, size = length(dfTrap$locx), replace = T)
 dfTrap$locy <- dfTrap$locy + sample(1:15, size = length(dfTrap$locy), replace = T)
 
 ## for behavioural data use 3m jitter
-dfBehav <- df2[data == "behaviour"]
+dfBehav <- df3[data == "behaviour"]
 dfBehav$locx <- dfBehav$locx + sample(1:3, size = length(dfBehav$locx), replace = T)
 dfBehav$locy <- dfBehav$locy + sample(1:3, size = length(dfBehav$locy), replace = T)
 
-df2 <- rbind(dfTrap, dfBehav)
+df3 <- rbind(dfTrap, dfBehav)
 
-saveRDS(df2, "output/spatial-locs.RDS")
+saveRDS(df3, "output/spatial-locs.RDS")
 
