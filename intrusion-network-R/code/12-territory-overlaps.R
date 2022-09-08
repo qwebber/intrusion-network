9843d900f7e52081e240bfbdafe3abe31be9cf2f
Quinn Webber <webber.quinn@gmail.com>
2022-09-08 11:59:30 -0400

    Update 12-territory-overlaps.R

--- intrusion-network-R/code/12-territory-overlaps.R
+++ intrusion-network-R/code/12-territory-overlaps.R
@@ -14,6 +14,48 @@ df$gr_year <- as.character(df$gr_year)
 ## prj
 prj <- '+init=epsg:26911'
 
+## calculate territory overlap for 30% kernels
+terr_over30 <- group_polys(df[gr_year == "KL_2002" | 
+                                gr_year == "KL_2004" |
+                                gr_year == "KL_2006" |
+                                gr_year == "KL_2009" |
+                                gr_year == "KL_2014" |
+                                gr_year == "KL_2020"], 
+                           area = TRUE,
+                           hrType = 'kernel',
+                           hrParams = list(percent = 30),
+                           id = 'squirrel_id',
+                           coords = c("locx", "locy"),
+                           projection = prj, 
+                           splitBy = c("grid", "year"))
+
+terr_over30$proportion <- round(terr_over30$proportion/10000, 3)
+terr_over30$area <- round(terr_over30$area/10000, 4)
+terr_over30[,same := (squirrel_id == squirrel_id2)]
+terr_over30 <- terr_over30[same != TRUE]
+terr_over30$percent <- 30
+
+## calculate territory overlap for 40% kernels
+terr_over40 <- group_polys(df[gr_year == "KL_2002" | 
+                                gr_year == "KL_2004" |
+                                gr_year == "KL_2006" |
+                                gr_year == "KL_2009" |
+                                gr_year == "KL_2014" |
+                                gr_year == "KL_2020"], 
+                           area = TRUE,
+                           hrType = 'kernel',
+                           hrParams = list(percent = 40),
+                           id = 'squirrel_id',
+                           coords = c("locx", "locy"),
+                           projection = prj, 
+                           splitBy = c("grid", "year"))
+
+terr_over40$proportion <- round(terr_over40$proportion/10000, 3)
+terr_over40$area <- round(terr_over40$area/10000, 4)
+terr_over40[,same := (squirrel_id == squirrel_id2)]
+terr_over40 <- terr_over40[same != TRUE]
+terr_over40$percent <- 40
+
 ## calculate territory overlap for 50% kernels
 terr_over50 <- group_polys(df[gr_year == "KL_2002" | 
                              gr_year == "KL_2004" |
@@ -121,7 +163,8 @@ terr_over90$percent <- 90
 
 
 ## bind together files
-terr_overlap_all <- rbind(terr_over50, terr_over60, 
+terr_overlap_all <- rbind(terr_over30, terr_over40, 
+                          terr_over50, terr_over60, 
                           terr_over70, terr_over80, 
                           terr_over90)
 terr_overlap_all$year <- as.factor(terr_overlap_all$year)
@@ -144,6 +187,53 @@ df_polys <- df[gr_year == "KL_2002" |
 
 yr <- data.table(gr_year = as.character(unique(df_polys$gr_year)))
 n = length(unique(yr$gr_year))
+
+
+# Generate 30% territorial polygons ---------------------------------
+out_polygon30 <- get_polygon(input = df_polys, 
+                             n = n,
+                             yr = yr,
+                             in.percent = 30,
+                             params = params)
+
+
+area30 <- c()
+for(i in 1:length(yr$gr_year)){ 
+  ar <- data.table(out_polygon30[[i]]$id_polygons, out_polygon30[[i]]$area)
+  ar$gr_year <- yr$gr_year[i]
+  setnames(ar, c("V1", "V2"), c("squirrel_id", "area_m2"))
+  
+  area30[[i]] <- ar
+  
+}
+
+## convert from list to data.table
+area30 <- rbindlist(area30)
+area30$percent <- 30
+
+
+# Generate 40% territorial polygons ---------------------------------
+out_polygon40 <- get_polygon(input = df_polys, 
+                             n = n,
+                             yr = yr,
+                             in.percent = 40,
+                             params = params)
+
+
+area40 <- c()
+for(i in 1:length(yr$gr_year)){ 
+  ar <- data.table(out_polygon40[[i]]$id_polygons, out_polygon40[[i]]$area)
+  ar$gr_year <- yr$gr_year[i]
+  setnames(ar, c("V1", "V2"), c("squirrel_id", "area_m2"))
+  
+  area40[[i]] <- ar
+  
+}
+
+## convert from list to data.table
+area40 <- rbindlist(area40)
+area40$percent <- 40
+
 
 # Generate 50% territorial polygons ---------------------------------
 out_polygon50 <- get_polygon(input = df_polys, 
@@ -256,6 +346,6 @@ for(i in 1:length(yr$gr_year)){
 area90 <- rbindlist(area90)
 area90$percent <- 90
 
-area_all <- rbind(area50, area60, area70, area80, area90)
+area_all <- rbind(area30, area40, area50, area60, area70, area80, area90)
 
 saveRDS(area_all, "output/territories/territory-size.RDS")
